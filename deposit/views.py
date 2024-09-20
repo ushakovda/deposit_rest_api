d58f5a1f5c3080252cd70395d3eb638c744d3553
ushakovda <ushakov328@gmail.com>
2024-09-20 15:53:26 +0700

    Added validation of errors in the request

--- deposit/views.py
+++ deposit/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime, timedelta
 from .serializers import DepositSerializer
+from rest_framework.exceptions import ParseError
 
 class DepositCalculationView(APIView):
     """Представление для расчета процентов по вкладу."""
@@ -16,23 +17,28 @@ class DepositCalculationView(APIView):
         Возвращает:
             Response: Объект ответа с результатами расчета или ошибками валидации.
         """
-        serializer = DepositSerializer(data=request.data)
-        if serializer.is_valid():
-            data = serializer.validated_data
-            start_date = datetime.strptime(data['date'], "%d.%m.%Y")  
-            periods = data['periods']
-            amount = data['amount'] 
-            rate = data['rate'] / 100 
+        try:
+            serializer = DepositSerializer(data=request.data)
+            if serializer.is_valid():
+                data = serializer.validated_data
+                start_date = datetime.strptime(data['date'], "%d.%m.%Y")  
+                periods = data['periods']
+                amount = data['amount'] 
+                rate = data['rate'] / 100 
 
-            result = {}
+                result = {}
 
-            for i in range(periods):
-                amount += amount * (rate / 12)  # Начисление процентов за месяц
-                
-                current_date = start_date + timedelta(days=30 * (i + 1))  # Дата через 30 дней
-                result[current_date.strftime("%d.%m.%Y")] = round(amount, 2)  # Сохранение результата
+                for i in range(periods):
+                    amount += amount * (rate / 12)  # Начисление процентов за месяц
+                    current_date = start_date + timedelta(days=30 * (i + 1))  # Дата через 30 дней
+                    result[current_date.strftime("%d.%m.%Y")] = round(amount, 2)  # Сохранение результата
 
-            return Response(result, status=status.HTTP_200_OK)
-        else:
-            error_message = "Описание ошибка"
-            return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(result, status=status.HTTP_200_OK)
+
+            else:
+                error_message = "Описание ошибка"
+                return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+        except (ValueError, ParseError):
+            # Если произошла ошибка при парсинге или валидации данных
+            return Response({"error": "Описание ошибка"}, status=status.HTTP_400_BAD_REQUEST)
