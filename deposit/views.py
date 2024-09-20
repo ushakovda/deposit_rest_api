8f7d4dac0d6f9b032cf971ffbb090a3087dc3ff5
ushakovda <ushakov328@gmail.com>
2024-09-20 14:39:30 +0700

    Creating a view class DepositCalculationView

--- deposit/views.py
+++ deposit/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime, timedelta
+from .serializers import DepositSerializer
 
-# Create your views here.
+class DepositCalculationView(APIView):
+    """Представление для расчета процентов по вкладу."""
+
+    def post(self, request) -> Response:
+        """Обработка POST-запроса для расчета процентов по вкладу.
+
+        Аргументы:
+            request: Объект запроса, содержащий данные о вкладе.
+
+        Возвращает:
+            Response: Объект ответа с результатами расчета или ошибками валидации.
+        """
+        serializer = DepositSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            start_date = datetime.strptime(data['date'], "%d.%m.%Y")  
+            periods = data['periods']
+            amount = data['amount'] 
+            rate = data['rate'] / 100 
+
+            result = {}
+
+            for i in range(periods):
+                amount += amount * (rate / 12)  # Начисление процентов за месяц
+                
+                current_date = start_date + timedelta(days=30 * (i + 1))  # Дата через 30 дней
+                result[current_date.strftime("%d.%m.%Y")] = round(amount, 2)  # Сохранение результата
+
+            return Response(result, status=status.HTTP_200_OK)
+        else:
+            error_message = "Описание ошибка"
+            return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
