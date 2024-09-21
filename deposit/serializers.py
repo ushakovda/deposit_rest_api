92cfa453cb493edbdcbf454a597f5c77896050eb
ushakovda <ushakov328@gmail.com>
2024-09-21 12:38:26 +0700

    Implemented functionality to save deposit applications to PostgreSQL database

--- deposit/serializers.py
+++ deposit/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+from .models import DepositApplication
 
-class DepositSerializer(serializers.Serializer):
-    """Сериализатор для данных о вкладе."""
 
-    date: str = serializers.CharField() 
-    periods: int = serializers.IntegerField(min_value=1, max_value=60)
-    amount: int = serializers.IntegerField(min_value=10000, max_value=3000000)
-    rate: float = serializers.FloatField(min_value=1, max_value=8)
+class DepositSerializer(serializers.ModelSerializer):
+    """Сериализатор для данных о вкладе и работы с моделью заявки."""
+
+    class Meta:
+        model = DepositApplication
+        fields = ["date", "periods", "amount", "rate"]
--- deposit/views.py
+++ deposit/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime, timedelta
 from .serializers import DepositSerializer
-from rest_framework.exceptions import ParseError
+
 
 class DepositCalculationView(APIView):
     """Представление для расчета процентов по вкладу."""
@@ -19,26 +19,30 @@ class DepositCalculationView(APIView):
         """
         try:
             serializer = DepositSerializer(data=request.data)
-            if serializer.is_valid():
-                data = serializer.validated_data
-                start_date = datetime.strptime(data['date'], "%d.%m.%Y")  
-                periods = data['periods']
-                amount = data['amount'] 
-                rate = data['rate'] / 100 
+            serializer.is_valid(
+                raise_exception=True
+            )  # Генерирует исключение при невалидных данных
+            data = serializer.validated_data
+            start_date = datetime.strptime(data["date"], "%d.%m.%Y")
+            periods = data["periods"]
+            amount = data["amount"]
+            rate = data["rate"] / 100
 
-                result = {}
+            result = {}
 
-                for i in range(periods):
-                    amount += amount * (rate / 12)  # Начисление процентов за месяц
-                    current_date = start_date + timedelta(days=30 * (i + 1))  # Дата через 30 дней
-                    result[current_date.strftime("%d.%m.%Y")] = round(amount, 2)  # Сохранение результата
+            for i in range(periods):
+                amount += amount * (rate / 12)  # Начисление процентов за месяц
+                current_date = start_date + timedelta(
+                    days=30 * (i + 1)
+                )  # Дата через 30 дней
+                result[current_date.strftime("%d.%m.%Y")] = round(
+                    amount, 2
+                )  # Сохранение результата
 
-                return Response(result, status=status.HTTP_200_OK)
+            serializer.save()
+            return Response(result, status=status.HTTP_200_OK)
 
-            else:
-                error_message = "Описание ошибка"
-                return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
-
-        except (ValueError, ParseError):
-            # Если произошла ошибка при парсинге или валидации данных
-            return Response({"error": "Описание ошибка"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(
+                {"error": "Описание ошибка"}, status=status.HTTP_400_BAD_REQUEST
+            )
